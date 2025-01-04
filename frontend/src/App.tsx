e1c625f184f284be7926f00e0dbb09ee26199490
t-hirata74 <t.hirata.0463@gmail.com>
2025-01-05 02:40:56 +0900

    deleteBookをApp.tsxに追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,34 @@
-import { useBooksQuery, useCreateBookMutation } from "./graphql/generated";
+import {
+  useBooksQuery,
+  useCreateBookMutation,
+  useDeleteBookMutation,
+} from "./graphql/generated";
 import { useState } from "react";
 
 function App() {
   const { data: { books = [] } = {} } = useBooksQuery();
   const [createBook] = useCreateBookMutation({ refetchQueries: ["books"] });
   const [title, setTitle] = useState("");
+  const [deleteBook] = useDeleteBookMutation({ refetchQueries: ["books"] });
 
   return (
     <div>
-    <input value={title} onChange={(e) => setTitle(e.target.value)} />
-    <button
-      onClick={() => {
-        createBook({ variables: { params: { title: title } } });
-        setTitle("");
-      }}
-    >
-      保存
-    </button>
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <button
+        onClick={() => {
+          createBook({ variables: { params: { title: title } } });
+          setTitle("");
+        }}
+      >
+        保存
+      </button>
       {books.map((book) => (
-        <div key={book.id}>{book.title}</div>
+         <div key={book.id}>
+           <div>{book.title}</div>
+           <button onClick={() => deleteBook({ variables: { id: book.id } })}>
+             削除
+           </button>
+         </div>
       ))}
     </div>
   );
