c04ec613caba437bc5a14217ce45e9be70762e30
t-hirata74 <t.hirata.0463@gmail.com>
2025-01-05 02:31:05 +0900

    App.tsxにcreateBookを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
-import { useBooksQuery } from "./graphql/generated";
+import { useBooksQuery, useCreateBookMutation } from "./graphql/generated";
+import { useState } from "react";
 
 function App() {
   const { data: { books = [] } = {} } = useBooksQuery();
+  const [createBook] = useCreateBookMutation({ refetchQueries: ["books"] });
+  const [title, setTitle] = useState("");
 
   return (
     <div>
+    <input value={title} onChange={(e) => setTitle(e.target.value)} />
+    <button
+      onClick={() => {
+        createBook({ variables: { params: { title: title } } });
+        setTitle("");
+      }}
+    >
+      保存
+    </button>
       {books.map((book) => (
         <div key={book.id}>{book.title}</div>
       ))}
